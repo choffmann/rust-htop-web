7363b0013477861c30e586b355486b33ddc4f078
Cedrik Hoffmann <dev@choffmann.io>
2025-02-23 16:32:44 +0100

    feat: print out cpu usage

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use axum::{routing::get, Router};
+use std::sync::{Arc, Mutex};
+
+use axum::{extract::State, routing::get, Router};
+use sysinfo::System;
 use tokio::net::TcpListener;
 
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>
+}
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(root_route));
+    let router = Router::new()
+        .route("/", get(root_route))
+        .route("/cpu", get(cpu_usage))
+        .with_state(AppState {
+            sys: Arc::new(Mutex::new(System::new_all()))
+        });
     let listener = TcpListener::bind("0.0.0.0:3123").await.unwrap();
 
     let addr = listener.local_addr().unwrap();
@@ -14,5 +26,19 @@ async fn main() {
 }
 
 async fn root_route() -> &'static str {
-    "Hello world!"
+    "Hello World!"
+}
+
+async fn cpu_usage(State(state): State<AppState>) -> String {
+    use std::fmt::Write; 
+    let mut s = String::new();
+    let mut sys = state.sys.lock().unwrap();
+    sys.refresh_cpu_usage();
+
+    for (i, cpu) in sys.cpus().iter().enumerate() {
+        let i = i + 1;
+        writeln!(s, "CPU {i}: {}%", cpu.cpu_usage()).unwrap();
+    }
+
+    s
 }
