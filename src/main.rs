fcece3f45ae27c35f0f6c9f32a423337ee4b7ba6
Cedrik Hoffmann <dev@choffmann.io>
2025-02-23 23:15:58 +0100

    feat: use broadcast to send cpu usage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 use axum::extract::ws::{Message, WebSocket};
@@ -9,18 +8,21 @@ use axum::Json;
 use axum::{extract::State, routing::get, Router};
 use sysinfo::{System, MINIMUM_CPU_UPDATE_INTERVAL};
 use tokio::net::TcpListener;
+use tokio::sync::broadcast;
 
+type Snapshot = Vec<f32>;
 
-type Cpus = Vec<f32>;
-
-#[derive(Default, Clone)]
+#[derive(Clone)]
 struct AppState {
-    cpus: Arc<Mutex<Cpus>>,
+    tx: broadcast::Sender<Snapshot>,
 }
 
 #[tokio::main]
 async fn main() {
-    let app_state = AppState::default();
+    let (tx, _) = broadcast::channel::<Snapshot>(1);
+    let app_state = AppState{
+        tx: tx.clone()
+    };
 
     let router = Router::new()
         .route("/", get(root_route))
@@ -35,17 +37,13 @@ async fn main() {
         loop {
             sys.refresh_cpu_usage();
             let usage: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
-            let mut cpus = app_state.cpus.lock().unwrap();
-            *cpus = usage;
-            drop(cpus);
+            let _ = tx.send(usage);
 
             std::thread::sleep(MINIMUM_CPU_UPDATE_INTERVAL)
         }
     });
 
-
     let listener = TcpListener::bind("0.0.0.0:3123").await.unwrap();
-
     let addr = listener.local_addr().unwrap();
     println!("Listen on {addr}");
 
@@ -74,8 +72,9 @@ async fn indexcss_route() -> impl IntoResponse {
 }
 
 async fn cpu_usage(State(state): State<AppState>) -> impl IntoResponse {
-    let usage = state.cpus.lock().unwrap().clone();
-    Json(usage)
+    let mut rx = state.tx.subscribe();
+    let value = rx.recv().await.unwrap_or(vec![]);
+    return Json(value)
 }
 
 async fn realtime_cpu_usage(ws: WebSocketUpgrade, State(state): State<AppState>) -> impl IntoResponse {
@@ -85,29 +84,11 @@ async fn realtime_cpu_usage(ws: WebSocketUpgrade, State(state): State<AppState>)
 }
 
 async fn realtime_cpu_stream(app_state: AppState, mut ws: WebSocket) {
-    loop {
-        let payload = serde_json::to_string(&*app_state.cpus.lock().unwrap()).unwrap();
+    let mut rx = app_state.tx.subscribe();
+    while let Ok(msg) = rx.recv().await {
+        let payload = serde_json::to_string(&msg).unwrap();
         ws.send(Message::Text(payload.into())).await.unwrap();
         tokio::time::sleep(Duration::from_millis(100)).await
     }
 }
 
-//async fn mem_usage(State(state): State<AppState>) -> impl IntoResponse {
-//    let mut s = String::new();
-//    let mut sys = state.sys.lock().unwrap();
-//    sys.refresh_memory();
-//
-//    fn to_gb(v: u64) -> f64 {
-//        v as f64 / (1024u64.pow(3) as f64)
-//    }
-//
-//    let total = to_gb(sys.total_memory());
-//    let free_mem = to_gb(sys.free_memory());
-//    let used_mem = to_gb(sys.used_memory());
-//
-//    writeln!(s, "Total: {} GB", total).unwrap();
-//    writeln!(s, "Free: {} GB", free_mem).unwrap();
-//    writeln!(s, "Used: {} GB", used_mem).unwrap();
-//
-//    s
-//}
